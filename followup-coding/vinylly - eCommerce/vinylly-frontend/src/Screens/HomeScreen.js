17c042712d202c32fc1dd511935a06ca05569a6f
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-08-25 13:46:39 +0700

    until React hook

--- followup-coding/vinylly - eCommerce/vinylly-frontend/src/Screens/HomeScreen.js
+++ followup-coding/vinylly - eCommerce/vinylly-frontend/src/Screens/HomeScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import data from '../data';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function HomeScreen (props) {
+
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get("/api/products");
+            setProduct(data);
+        }
+        fetchData();
+        return () => {
+            //
+        };
+    }, [])
+
     return <ul className="products">
-                {data.products.map(product => 
+                {products.map(product => 
                 <li>
                     <div className="product">
                         <Link to={'/product/' + product._id}>
