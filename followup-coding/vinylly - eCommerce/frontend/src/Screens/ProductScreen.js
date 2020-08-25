b53a5736c8011de71509e0801563deba40b92d0a
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-08-25 22:00:29 +0700

    test1

--- followup-coding/vinylly - eCommerce/frontend/src/Screens/ProductScreen.js
+++ followup-coding/vinylly - eCommerce/frontend/src/Screens/ProductScreen.js
@@ -1,69 +1,83 @@
-import React from 'react'
-import data from '../data';
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen (props) {
 
-    const product = data.products.find(x => x._id === props.match.params.id);
+    const productDetails = useSelector(state => state.productDetails)
+    const {product, loading, error} = detailsProduct;
+    const dispatch = useDispatch;
 
-    const trackListing =  product.tracklist.map(track =>
-        <li key={track}>{track}</li>
-    );
+    useEffect(() => {
+        dispatch(detailsProduct(props.match.params.id));
+        return () => {
+            //
+        };
+    }, [])
+
+    // const trackListing =  product.tracklist.map(track =>
+    //     <li key={track}>{track}</li>
+    // );
 
     return <div>
                 <div className="back-to-result">
                     <Link to="/">Back to result...</Link>
                 </div>
 
-                <div className="details">
-                    <div className="details-image">
-                        <img src={product.image} alt="vinyl" />
-                    </div>
-                    <div className="details-info">
-                        <ul>
-                            <li>
-                                <h1>{product.name}</h1>
-                            </li>
-                            <li>
-                                {product.artist}
-                            </li>
-                            <li>
-                                {product.rating} stars ({product.numReview} reviews)
-                            </li>
-                            <li>
-                                <b>Price: ${product.price}</b>
-                            </li>
-                            <li>
-                                Tracklist: <ol>
-                                    {trackListing}
-                                </ol>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div className="details-action">
-                        <ul>
-                            <li>
-                                Price: {product.price}
-                            </li>
-                            <li>
-                                Status: {product.status}
-                            </li>
-                            <li>
-                                Qty: <select>
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
-                            </li>
-                            <li>
-                                <button className="button primary">Add to cart</button>
-                            </li>
-                        </ul>
-                    </div>
+                {loading? <div>loading...</div>
+                : error? <div>{error}</div>
+                : (<div className="details">
+                <div className="details-image">
+                    <img src={product.image} alt="product"></img>
                 </div>
+                <div className="details-info">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            {product.artist}
+                        </li>
+                        <li>
+                            {product.rating} stars ({product.numReview} reviews)
+                        </li>
+                        <li>
+                            <b>Price: ${product.price}</b>
+                        </li>
+                        <li>
+                            Tracklist: <ol>
+                                {/* {trackListing} */}
+                            </ol>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="details-action">
+                    <ul>
+                        <li>
+                            Price: {product.price}
+                        </li>
+                        <li>
+                            Status: {product.status}
+                        </li>
+                        <li>
+                            Qty: <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </li>
+                        <li>
+                            <button className="button primary">Add to cart</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>)
+            }
+                
             </div>
 }
 
