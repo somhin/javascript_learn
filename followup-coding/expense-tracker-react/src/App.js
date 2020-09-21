d84585a6d40da966c7a2268c217d8a494059e55b
Phipat Limprasutr <phipat.limprasutr@outlook.com>
2020-09-21 23:38:28 +0700

    finish

--- followup-coding/expense-tracker-react/src/App.js
+++ followup-coding/expense-tracker-react/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses'
+import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
+
+import { GlobalProvider } from './context/GlobalStates'
 
 function App() {
   return (
-    <div>
-      DUDE
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
